e137b7a8ff83f0d1276e8d7463d321ee880b2a53
bote795 <nbotell2@gmail.com>
2016-06-02 23:26:52 -0500

    retrieve basic data functions

--- index.js
+++ index.js
@@ -7,3 +7,65 @@ nani.get('user/'+username+'/animelist')
   .catch(error => {
     console.log(error);
   });
+function  ListsOfAnime (userData){
+ var completed = userData.lists.completed;
+ var dropped = userData.lists.dropped;
+ var on_hold = userData.lists.on_hold;
+ var watching = userData.lists.watching;
+ array.forEach(function(animeItem){
+ 	return animeItem.anime.id;
+ });
+}
+//genres
+function retrieveGenres(id){
+	var deferred = $.Deferred();
+	nani.get('anime/'+id)
+	  .then(data => {
+	  	console.log(data);
+	    deferred.resolve({data});
+	  })
+	  .catch(error => {
+	    console.log(error);
+	  });
+	 return deferred.promise();
+}
+
+function retrieveStaff(id) {
+	var deferred = $.Deferred();
+	nani.get('anime/'+id+'/staff')
+	  .then(data => {
+	  	console.log(data);
+	    deferred.resolve({data});
+	  })
+	  .catch(error => {
+	    console.log(error);
+	  });
+	 return deferred.promise();	
+}
+
+function retrieveActors(id) {
+	var deferred = $.Deferred();
+	nani.get('anime/'+id+'/actors')
+	  .then(data => {
+	    deferred.resolve({genres: data.genres});
+	  })
+	  .catch(error => {
+	    console.log(error);
+	  });
+	 return deferred.promise();	
+}
+
+function checkForAnimeDetails (animeIds,callback) {
+  var promises=[];
+  for (var i = 0; i < animeIds.length; i++) {
+    promises.push( retrieveGenres(animeIds[i]) );
+    promises.push(retrieveStaff(nimeIds[i]));
+    promises.push( retrieveActors(animeIds[i]));
+  };
+  $.when.apply($, promises).then(function() {
+    var temp=arguments; // The array of resolved objects as a pseudo-array
+    callback(temp);
+  });
+}
+retrieveStaff(18679);
+retrieveActors(18679);
