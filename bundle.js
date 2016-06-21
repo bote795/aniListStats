a3e2a4858d839bfa097a8fff2b791f06179bc037
bote795 <nbotell2@gmail.com>
2016-06-21 11:18:38 -0500

    retrieve one list anime user data

--- bundle.js
+++ bundle.js
@@ -4,18 +4,22 @@ var username = "bote795";
 nani.get('user/'+username+'/animelist')
   .then(data => {
     console.log(data);
+    ListsOfAnime(data);
   })
   .catch(error => {
     console.log(error);
   });
 function  ListsOfAnime (userData){
+ var animeLists = new Map();
+ var promises = [];
  var completed = userData.lists.completed;
  var dropped = userData.lists.dropped;
  var on_hold = userData.lists.on_hold;
  var watching = userData.lists.watching;
- array.forEach(function(animeItem){
+ var watchList = watching.map(function(animeItem){
  	return animeItem.anime.id;
  });
+checkForAnimeDetails("watching",watchList,processInfo);
 }
 //genres
 function retrieveGenres(id){
@@ -23,8 +27,7 @@ function retrieveGenres(id){
 	nani.get('anime/'+id)
 	  .then(data => {
 	  	console.log(data);
-	  	console.log("genres", data.genres);
-	    deferred.resolve({genres: data.genres});
+	    deferred.resolve({genres: data.genres , action: "genres", id: id});
 	  })
 	  .catch(error => {
 	    console.log(error);
@@ -45,7 +48,7 @@ function retrieveStaff(id) {
 	  		};
 	  	});
 	  	console.log("staff ",staff);
-	    deferred.resolve({data});
+	    deferred.resolve({staff:  staff, action: "staff", id: id});
 	  })
 	  .catch(error => {
 	    console.log(error);
@@ -53,19 +56,67 @@ function retrieveStaff(id) {
 	 return deferred.promise();	
 }
 
-function checkForAnimeDetails (animeIds,callback) {
+/**
+ * checkForAnimeDetails creates and sends all primses
+ * @param  {array}   animeIds  an array of anime Ids
+ * @param  {Function} callback function that takes in an array of results
+ * @return {[type]}            [description]
+ */
+function checkForAnimeDetails (name,animeIds,callback) {
   var promises=[];
   for (var i = 0; i < animeIds.length; i++) {
     promises.push( retrieveGenres(animeIds[i]) );
-    promises.push(retrieveStaff(nimeIds[i]));
+    promises.push(retrieveStaff(animeIds[i]));
   };
   $.when.apply($, promises).then(function() {
     var temp=arguments; // The array of resolved objects as a pseudo-array
-    callback(temp);
+    callback(name,temp);
   });
 }
-retrieveGenres(18679);
-retrieveStaff(18679);
+/**
+ * processInfo fuse genres and staff to one item
+ * @param  {[type]} data [description]
+ * @return {[type]}      [description]
+ */
+function processInfo(name,data){
+	var deferred = $.Deferred();
+	console.log(data);
+	var animeList = new Map();
+	for(var i = 0; i < data.length; i++)
+	{
+		var item = data[i];
+		switch(item.action){
+			case "genres":
+				if(!animeList.has(item.id))
+				{
+					var anime = {id: item.id, genres: item.genres};
+					animeList.set(item.id, anime);
+				}
+				else
+				{
+					var anime = animeList.get(item.id);
+					anime.genres = item.genres;
+					animeList.set(item.id, anime);
+				}
+			break;
+			case "staff":
+				if(!animeList.has(item.id))
+				{
+					var anime = {id: item.id, staff: item.staff};
+					animeList.set(item.id, anime);
+				}
+				else
+				{
+					var anime = animeList.get(item.id);
+					anime.staff = item.staff;
+					animeList.set(item.id, anime);
+				}
+			break;
+		}
+	}
+	deferred.resolve({name: name,map: animeList});
+	return deferred.promise();
+}
 },{"nani":2}],2:[function(require,module,exports){
 'use strict';
 
