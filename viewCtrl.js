e215e3b52a33720a58e25938afbe6c1e9e245b3e
bote795 <nbotell2@gmail.com>
2016-07-03 16:51:06 -0500

    change format and add datatables

--- viewCtrl.js
+++ viewCtrl.js
@@ -18,19 +18,19 @@
 			  	window.anime(username).then(function(){
 				  	//studio pie Chart
 				  	var orderedStudios = Stats.getSortedMap("studio");
-				  	for (var i = 0; i < 6; i++) {
+				  	for (var i = 0; i < orderedStudios.length; i++) {
 				  		vm.studios.push({label: Stats.studiosNamesMap.get(orderedStudios[i][0] ).studio_name, value: orderedStudios[i][1]});
 				  	}
 
 
 				  	//staff pie chart
 				  	var orderedStaff = Stats.getSortedMap("staff");
-				  	for (var i = 0; i < 6; i++) {
+				  	for (var i = 0; i < orderedStaff.length; i++) {
 				  		vm.staff.push({label: formatStaffName(Stats.staffNamesMap.get(orderedStaff[i][0])), value: orderedStaff[i][1], role:  Stats.staffNamesMap.get(orderedStaff[i][0]).role});
 				  	}
 
 				  	var orderedGenre = Stats.getSortedMap("genres");
-				  	for (var i = 0; i < 6; i++) {
+				  	for (var i = 0; i < orderedGenre.length; i++) {
 				  		vm.genre.push({label: orderedGenre[i][0], value: orderedGenre[i][1]});
 				  	}
 			  	});
